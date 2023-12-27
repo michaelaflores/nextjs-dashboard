44f199cdbcf5c040fc3624d759b855021aebaa16
Michael Flores <mike@michaelflores.io>
2023-12-27 15:47:32 -0500

    Example of streaming with suspense

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,16 +1,14 @@
-import {
-  fetchCardData,
-  fetchLatestInvoices,
-  fetchRevenue,
-} from '@/app/lib/data';
+import { Suspense } from 'react';
+
+import { fetchCardData, fetchLatestInvoices } from '@/app/lib/data';
 import { Card } from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const [revenue, latestInvoices, cardData] = await Promise.all([
-    fetchRevenue(),
+  const [latestInvoices, cardData] = await Promise.all([
     fetchLatestInvoices(),
     fetchCardData(),
   ]);
@@ -38,7 +36,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
